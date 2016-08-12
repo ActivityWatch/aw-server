c4e78552b828cdee1720c955a944998c604d47e1
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-08-12 14:22:55 +0200

    Added dependency link to aw-core

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(name='aw-server',
       url='https://github.com/ActivityWatch/aw-server',
       packages=['aw_server'],
       install_requires=['aw-core', 'flask>=0.10', 'flask-restplus>=0.9.2', 'flask-cors>=2.1', 'pymongo>=3.2', 'appdirs==1.4.0', 'python-json-logger>=0.1.5'],
+      dependency_links=[
+          'https://github.com/ActivityWatch/aw-core/tarball/master#egg=aw-core'
+      ],
       entry_points={
-            'console_scripts': ['aw-server = aw_server:main']
-        }
-     )
+          'console_scripts': ['aw-server = aw_server:main']
+      })

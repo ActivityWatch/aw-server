37a3a13b2d74ba3e41814956a582f0713203bba6
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-08-29 18:48:17 +0200

    Fixed tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,19 +5,19 @@ from datetime import datetime
 from aw_client import ActivityWatchClient
 from aw_core.models import Event
 
-from aw_datastore import Datastore
-
 logging.basicConfig(level=logging.WARN)
+
 
 class ClientTest(unittest.TestCase):
     def setUp(self):
         self.client = ActivityWatchClient("unittest", testing=True)
+        self.client.create_bucket("test", "testevents")
 
     def test_send_event(self):
-        self.client.send_event(Event(timestamp=datetime.now(), label="test"))
+        self.client.send_event("test", Event(timestamp=datetime.now(), label="test"))
 
     def test_send_events(self):
-        self.client.send_events([
+        self.client.send_events("test", [
             Event(timestamp=datetime.now(), label="test"),
             Event(timestamp=datetime.now(), label="test2"),
         ])

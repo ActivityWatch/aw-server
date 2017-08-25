b0807b9e8fbafee5b9d6f7166c60c6e156e37918
Erik Bjäreholt <erik@bjareho.lt>
2017-08-25 17:07:07 +0200

    added setUp and tearDown functions to benchmark

--- scripts/benchmark-api-heartbeat.py
+++ scripts/benchmark-api-heartbeat.py
@@ -9,20 +9,22 @@ from aw_core.models import Event
 import aw_datastore
 import aw_server
 
+ds = aw_datastore.Datastore(aw_datastore.storages.PeeweeStorage, testing=True)
+api = aw_server.api.ServerAPI(ds, testing=True)
 
-def benchmark():
-    ds = aw_datastore.Datastore(aw_datastore.storages.PeeweeStorage, testing=True)
-    api = aw_server.api.ServerAPI(ds, testing=True)
+print(api.get_info())
+bucket_id = "test-benchmark"
 
-    print(api.get_info())
-
-    bucket_id = "test-benchmark"
-
+def setUp():
     try:
         api.create_bucket(bucket_id, "test", "test", "test")
     except Exception as e:
         print(e)
 
+def tearDown():
+    api.delete_bucket(bucket_id)
+
+def benchmark():
     print("Benchmarking... this will take 30 seconds")
     for i in range(120):
         sleep(0.1)
@@ -30,7 +32,11 @@ def benchmark():
 
 if __name__ == "__main__":
     f_bench = "benchmark.dat"
+
+    setUp()
     cProfile.run('benchmark()', f_bench)
+    tearDown()
+
     p = pstats.Stats(f_bench)
     p.strip_dirs()
     # p.sort_stats('tottime')

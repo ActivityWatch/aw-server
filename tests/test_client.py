ceb08e8c786440b07ffd5a103efcebfbd373f4e6
Stephen Cline <8979071+sacline@users.noreply.github.com>
2018-03-25 13:05:27 -0500

    updated tests to work with future events filter

--- tests/test_client.py
+++ tests/test_client.py
@@ -161,7 +161,7 @@ def test_send_event(client, bucket):
 
 
 def test_send_events(client, bucket):
-    events = _create_periodic_events(2)
+    events = _create_periodic_events(2, start=datetime.now(tz=timezone.utc) - timedelta(days=1))
 
     client.send_events(bucket, events)
     recv_events = client.get_events(bucket)
@@ -171,7 +171,7 @@ def test_send_events(client, bucket):
 
 
 def test_get_events_interval(client, bucket):
-    start_dt = datetime.now(tz=timezone.utc)
+    start_dt = datetime.now(tz=timezone.utc) - timedelta(days=2)
     delta = timedelta(hours=1)
     events = _create_periodic_events(1000, delta=delta, start=start_dt)
 
@@ -185,7 +185,7 @@ def test_get_events_interval(client, bucket):
 
 
 def test_store_many_events(client, bucket):
-    events = _create_periodic_events(1000)
+    events = _create_periodic_events(1000, start=datetime.now(tz=timezone.utc) - timedelta(days=50))
 
     client.send_events(bucket, events)
     recv_events = client.get_events(bucket, limit=-1)
@@ -195,9 +195,9 @@ def test_store_many_events(client, bucket):
 
 
 def test_midnight(client, bucket):
-    now = datetime.now()
-    midnight = now.replace(hour=23, minute=50)
-    events = _create_periodic_events(100, start=midnight, delta=timedelta(minutes=1))
+    start_dt = datetime.now() - timedelta(days=1)
+    midnight = start_dt.replace(hour=23, minute=50)
+    events = _create_periodic_events(100, start=start_dt, delta=timedelta(minutes=1))
 
     client.send_events(bucket, events)
     recv_events = client.get_events(bucket, limit=-1)
@@ -223,10 +223,13 @@ def test_midnight_heartbeats(client, bucket):
 
 
 def test_filter_future_events(client, bucket):
-    now = datetime.now(tz=timezone.utc)
-    future_events = _create_periodic_events(100, start=now, delta=timedelta(minutes=1))
+    future_start = datetime.now(tz=timezone.utc) + timedelta(minutes=1)
+    future_events = _create_periodic_events(100, start=future_start, delta=timedelta(minutes=1))
 
-    client.send_events(bucket, future_events)
+    #All events take place in the future, so an exception will be raised
+    with pytest.raises(Exception):
+        client.send_events(bucket, future_events)
+
     recv_events = client.get_events(bucket, limit=-1)
 
     assert len(recv_events) == 0

416705d82595076aed395fdfda296a8389dd935a
Erik Bjäreholt <erik@bjareho.lt>
2018-05-28 17:10:03 +0200

    added autodetection of version

--- aw_server/__about__.py
+++ aw_server/__about__.py
@@ -1,1 +1,24 @@
-__version__ = "0.8.0-dev"
+import os
+import subprocess
+
+
+def detect_version():
+    basever = "v0.8"
+
+    for env_var in ["TRAVIS_TAG", "APPVEYOR_REPO_TAG_NAME"]:
+        if env_var in os.environ:
+            return os.environ[env_var]
+
+    for env_var in ["TRAVIS_COMMIT", "APPVEYOR_REPO_COMMIT"]:
+        if env_var in os.environ:
+            return basever + "-" + os.environ[env_var]
+
+    try:
+        return basever + "-dev-" + str(subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip(), "utf8")
+    except Exception as e:
+        print("Unable to get current commit with git")
+
+    return basever + "-dev-unknown"
+
+
+__version__ = detect_version()

5f42ca64096d46291e091e62a189f8608c07c0da
Erik Bjäreholt <erik@bjareho.lt>
2021-06-16 12:30:56 +0200

    fix: improved version detection/assignment

--- aw_server/__about__.py
+++ aw_server/__about__.py
@@ -3,12 +3,51 @@ import os
 import subprocess
 import pkg_resources
 import re
+import logging
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+# TODO: Remove reliance on
+basever = "v0.11"
+
+srcpath = Path(__file__).absolute().parent
+projectpath = srcpath.parent
+bundlepath = projectpath.parent  # the ActivityWatch bundle repo, in some circumstances
+
+# This line set by script when run (metaprogramming)
+__version__ = "v0.11.0b1.dev+98fd120"
 
 
-basever = "v0.10"
+def get_rev():
+    p = subprocess.run(
+        "git rev-parse HEAD",
+        shell=True,
+        capture_output=True,
+        cwd=workdir,
+    )
+    return p.stdout.strip()
 
 
 def detect_version_ci() -> Optional[str]:
+    # always set to true in GitHub actions
+    if os.environ.get("CI", False) == "true":
+        # GitHub Actions build
+        rev = get_rev()
+        p = subprocess.run(
+            ["git", "describe", "--tags", "--abbrev=0", "--exact-match", rev],
+            capture_output=True,
+            encoding="utf8",
+            cwd=workdir,
+        )
+        if p.stderr:
+            if "no tag exactly matches" in p.stderr:
+                pass
+            else:
+                raise Exception(p.stderr)
+        else:
+            return p.stdout.strip()
+
     for env_var in ["TRAVIS_TAG", "APPVEYOR_REPO_TAG_NAME"]:
         if env_var in os.environ:
             return os.environ[env_var]
@@ -21,18 +60,24 @@ def detect_version_ci() -> Optional[str]:
 
 def detect_version_git() -> Optional[str]:
     try:
+        tag = subprocess.check_output(
+            ["git", "describe", "--abbrev=0", "--tags"], encoding="utf8", cwd=workdir
+        ).strip()
+        if tag:
+            basever = tag
         return (
             basever
             + ".dev+"
             + str(
                 subprocess.check_output(
-                    ["git", "rev-parse", "--short", "HEAD"]
+                    ["git", "rev-parse", "--short", "HEAD"], cwd=workdir
                 ).strip(),
                 "utf8",
             )
         )
-    except Exception:
+    except Exception as e:
         # Unable to get current commit with git
+        logger.exception(e)
         return None
 
 
@@ -52,14 +97,11 @@ def detect_version():
     return basever + ".dev+unknown"
 
 
-__version__ = "v0.10.dev+32e3624"
-
-
 def assign_static_version():
     """Self-modifies the current script to lock in version"""
     version = detect_version()
     with open(__file__, "r") as f:
-        versionline = "\n__version__ = '{}'".format(version)
+        versionline = '\n__version__ = "{}"'.format(version)
         data = re.sub(r"\n__version__ = [^\n;]+", versionline, f.read())
 
     with open(__file__, "w") as f:
@@ -69,4 +111,16 @@ def assign_static_version():
 
 
 if __name__ == "__main__":
+    # Use the bundle repo as working repo, if it exists
+    if (bundlepath / ".git").exists():
+        print("Found bundle repo in parent dir, using for calls to git")
+        workdir = bundlepath
+    else:
+        workdir = projectpath
+
     assign_static_version()
+
+    print("By method:")
+    print(f"  git: {detect_version_git()}")
+    print(f"  ci:  {detect_version_ci()}")
+    print(f"  pkg: {detect_version_pkg()}")

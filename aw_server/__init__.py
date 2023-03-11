5b8f294947710a43dfb24e27291fd875b1c4cd30
Erik Bjäreholt <erik@bjareho.lt>
2023-03-11 11:32:25 +0100

    fix: added ruff linting, fixed lints

--- aw_server/__init__.py
+++ aw_server/__init__.py
@@ -2,12 +2,10 @@ import logging as _logging
 
 logger = _logging.getLogger(__name__)
 
-from . import __about__
 from .__about__ import __version__
-
-from .server import create_app
-
-from . import api
-from . import rest
-
 from .main import main
+
+__all__ = [
+    "__version__",
+    "main",
+]

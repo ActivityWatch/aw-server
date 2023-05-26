df76a778596cb65dcca174c1f80a5ff04077c829
Erik Bjäreholt <erik@bjareho.lt>
2023-05-26 11:27:52 +0200

    fix: removed MANIFEST.in (replaced with pyproject.toml include)

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Used to create base .spec (needs manual modification)
-# pyinstaller __main__.py -n aw-server
-
-echo "Please make sure that ./aw_server/static contains the build from aw-webui"
-
-pyinstaller aw-server.spec
